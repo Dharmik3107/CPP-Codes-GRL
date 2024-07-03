2cde641b21d5a2e1b53b3d6f293b149d24045668
Dharmik3107 <abhangidharmik@gmail.com>
2024-07-03 15:16:11 +0530

    Tutorial9 Added

---
+++ tutorial9.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //! For loops
+
+    for (int i = 0; i <= 21; i++) // i++ => i = i + 1
+    {
+        cout << i << endl;
+        // i = i + 7; // to skip given number step
+    }
+    // for(int i = 10; i > 0; i--){
+
+    // }
+
+    return 0;
+}
